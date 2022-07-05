ad2d0529bc9723ae09ef795e2eb07e04c857d417
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-05 15:21:31 +0100

    setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <main id="home">
+        Home
+    </main>
+  )
+}
+
+export default Home
